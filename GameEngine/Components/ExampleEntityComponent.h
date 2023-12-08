9097301ec6b73603d0696b0bee1f3bcc8fba98a9
PHallin <petter-hallin99@live.se>
2023-12-08 12:27:31 +0100

    Created Example Component class

---
+++ GameEngine/Components/ExampleEntityComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "IComponent.h"
+
+/**
+ * \brief Example Component :D
+ */
+class EntityComponent final : public IComponent {
+public:
+    explicit EntityComponent(std::string name);
+    void Start() override;
+    void Execute() override;
+    void Stop() override;
+    void InitializeComponent() override;
+    std::string GetName() override;
+};
